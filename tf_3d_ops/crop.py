1cb86765e923d2189c4ae2d06a368f42310c601e
Muhammad Anas Raza <memanasraza@gmail.com>
2023-02-09 21:44:06 -0500

    Added code for cropping

--- tf_3d_ops/crop.py
+++ tf_3d_ops/crop.py
@@ -1,1 +1,9 @@
+import tensorflow as tf
+def _crop_unbatched(image, start, size):
+    d, h, w = start
+    i, j, k = size
 
+    return image[d:d+i, h:h+j, w:w+k]
+
+def _crop_batched(images, start, size):
+    return tf.map_fn(lambda x: _crop_unbatched(x, start, size), images)
